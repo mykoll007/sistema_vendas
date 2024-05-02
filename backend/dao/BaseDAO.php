6fa9faa262211bbd1518eaae8dc12c1f2209d66c
aeciobrito <aeciobrito@gmail.com>
2024-05-02 20:43:32 -0300

    Base Data Access Object

---
+++ backend/dao/BaseDAO.php
@@ -0,0 +1,8 @@
+<?php
+    interface BaseDAO {
+        public function getById($id);
+        public function getAll();
+        public function create($entity);
+        public function update($entity);
+        public function delete($id);
+    }
