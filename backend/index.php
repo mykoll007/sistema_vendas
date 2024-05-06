4e86900ddd66256d1a61c48fac08eb24caa6a7af
aeciobrito <aeciobrito@gmail.com>
2024-05-06 20:41:35 -0300

    limpeza do index.php

--- backend/index.php
+++ backend/index.php
@@ -1,20 +1,5 @@
 <?php
     require_once("template/header.php");
-    require_once 'dao/UsuarioDAO.php';
-    require_once 'entity/Usuario.php';
-
-    $usuarioDao = new UsuarioDAO();
-    //echo $usuarioDao->getById(1)->getNomeUsuario();
-    // print_r($usuarioDao->getAll());
-
-    // $novoUsuario = new Usuario(null, "novo usuario B", "1234aerqe", "novouser@mail.com", null, 1);
-    // echo $novoUsuario->getNomeUsuario();
-
-    // $usuarioDao->create($novoUsuario);
-    $novoUsuario = new Usuario(2, "Clayton", "1234aerqe", "novouser@mail.com", null, 1);
-    $usuarioDao->update($novoUsuario);
-  
-
 ?>
     <h1>Olá Sistema Vendas Body</h1>
 </body>
