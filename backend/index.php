2c6b31af39a3897c49202136a804d07191597686
aeciobrito <aeciobrito@gmail.com>
2024-05-06 20:05:55 -0300

    BaseDAO crete - troca de valor por referencia

--- backend/index.php
+++ backend/index.php
@@ -1,11 +1,16 @@
 <?php
     require_once("template/header.php");
     require_once 'dao/UsuarioDAO.php';
+    require_once 'entity/Usuario.php';
 
     $usuarioDao = new UsuarioDAO();
-    $usuario = $usuarioDao->getById(1);
-    $x = $usuario->getSenha();
-    echo "$x";
+    // print_r($usuarioDao->getAll());
+
+    $novoUsuario = new Usuario(null, "novo usuario B", "1234aerqe", "novouser@mail.com", null, 1);
+    echo $novoUsuario->getNomeUsuario();
+
+    $usuarioDao->create($novoUsuario);
+  
 
 ?>
     <h1>Olá Sistema Vendas Body</h1>
